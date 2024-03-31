e15b5e24e29236a1c8ec31618f38b61323f5219a
YCLin win11 <a50206123@gmail.com>
2024-03-31 23:10:14 +0800

    Adjust main gui layout

--- cmd_main.py
+++ cmd_main.py
@@ -16,7 +16,7 @@ def connect_etabs(self) :
         self.btn_connect.configure(text = f"Disconnect to {self.etabs.EDB_name}!! Click to connect again !! ")
         self.etabs = None
 
-        self.label_show_path.configure(text = '')
+        self.label_show_path.configure(text = ' ')
         
     done_msg(msg)
 
@@ -83,7 +83,7 @@ def reduction_torsion(self, reduction = 0.1) : # need to test
     
     for frame, story in frames :
         if frame[0] == 'B' :
-            self.etabs.Frame.set_modifier(frame, T = reduction)
+            self.etabs.Frames.set_modifier(frame, T = reduction)
     
     self.etabs.refresh()
     
@@ -103,7 +103,9 @@ def set_rz(self, rz = 0.5, frame_prefix = ['B', 'C', 'D']) : # need to test
         if self.etabs.Frames.get_section(frame)[0] in frame_prefix :
             self.etabs.set_rigidzone(frame, rz)
         else :
-            self.etabs.set_rigidzone(frame, 0.0)
+            self.etabs.Frames.set_rigidzone(frame, 0.0)
+    
+    self.etabs.refresh()
     
     done_msg(msg)
     
@@ -122,6 +124,8 @@ def sb_nonsway(self, frame_prefix = ['F', 'S']) : # need to test
             self.etabs.Design.ConcFrame.set_overwrite('', 0, 0, quick = 'nonsway')
         else :
             self.etabs.Design.ConcFrame.set_overwrite('', 0, 0, quick = 'sway')
+    
+    self.etabs.refresh()
     
     done_msg(msg)
     
--- gui_main.py
+++ gui_main.py
@@ -3,7 +3,9 @@ import customtkinter as ctk
 import tkinter as tk
 from yc_etabs_api.etabs import ETABS
 from gui_loading import LoadingUI
-from cmd_section import SectionUI
+# from cmd_section import SectionUI
+
+ui_width = 621
 
 import cmd_main as cmd
 
@@ -12,46 +14,72 @@ class CommonOperationUI :
         self.etabs = None
 
         self.initUI()
-        
-        self.app.mainloop()
+
     
     def initUI(self) :
         ctk.set_appearance_mode('dark') 
-        # ctk.set_default_color_theme()
+        ctk.set_default_color_theme("dark-blue")
 
         app = ctk.CTk()
         app.geometry('640x480')
         app.title('ETABS Faster by YuChen Lin')
+
+        frame_1 = ctk.CTkFrame(app, width=ui_width)
+        frame_1.pack()
+        frame_2 = ctk.CTkFrame(app, width=ui_width)
+        frame_2.pack()
+
+        #### Frame-1
+        label_show_path = ctk.CTkLabel(frame_1, text=" ", width=ui_width)
+        label_show_path.pack()
         
-        app.grid_rowconfigure(0,)
-        app.grid_rowconfigure(1,)
-        app.grid_rowconfigure(2)
-        app.grid_rowconfigure(3)
-        app.grid_rowconfigure(4)
-        app.grid_columnconfigure(0, weight=3)
-        app.grid_columnconfigure(1, weight=0)
-        app.grid_columnconfigure(2, weight=3)
+        btn_connect = ctk.CTkButton(frame_1, text="Connect to ETABS", command=self.connect_etabs, width=ui_width)
+        btn_connect.pack()
 
-        frame_0 = ctk.CTkFrame(app)
+        #### Frame-2
+        ##### Frame 2-1
+        frame_2_1 = ctk.CTkFrame(frame_2, width=ui_width/3)
+        frame_2_1.grid(row=0, column=0)
+        ##### Frame-2-1-1
+        label_2_1_1 = ctk.CTkLabel(frame_2_1, text='Fast Release')
+        label_2_1_1.pack()
+        frame_2_1_1 = ctk.CTkFrame(frame_2_1)
+        frame_2_1_1.pack()
 
-        btn_connect = ctk.CTkButton(app, text="Connect to ETABS", width=600, command=self.connect_etabs)
-        btn_connect.grid(row=0, column=0, columnspan = 3, padx=20, pady=(20, 0))
-        
-        label_show_path = ctk.CTkLabel(app, text="")
-        label_show_path.grid(row=1, column=0, columnspan = 3, padx=20, pady=0)
+        btn_release_i = ctk.CTkButton(frame_2_1_1, text="Release I", command=self.release_i, height = 20, width=ui_width/3)
+        btn_release_i.grid(row=0, column=0, padx=0, pady=0)
+        btn_release_j = ctk.CTkButton(frame_2_1_1, text="Release J", command=self.release_j, height = 20, width=ui_width/3)
+        btn_release_j.grid(row=1, column=0, padx=0, pady=0)
+        btn_release_ij = ctk.CTkButton(frame_2_1_1, text="Release IJ", command=self.release_ij, height = 20, width=ui_width/3)
+        btn_release_ij.grid(row=2, column=0, padx=0, pady=0)
 
-        btn_release_i = ctk.CTkButton(app, text="Release I", command=self.release_i, height = 20)
-        btn_release_i.grid(row=2, column=0, padx=0, pady=5)
-        btn_release_j = ctk.CTkButton(app, text="Release J", command=self.release_j, height = 20)
-        btn_release_j.grid(row=2, column=1, padx=0, pady=5)
-        btn_release_ij = ctk.CTkButton(app, text="Release IJ", command=self.release_ij, height = 20)
-        btn_release_ij.grid(row=2, column=2, padx=0, pady=5)
+        ##### Frame-2-1-2
+        label_2_1_2 = ctk.CTkLabel(frame_2_1, text='\nAlways Do those')
+        label_2_1_2.pack()
+        frame_2_1_2 = ctk.CTkFrame(frame_2_1)
+        frame_2_1_2.pack()        
 
-        btn_loading = ctk.CTkButton(app, text="Click Me to Loading", command=self.open_loading)
+        btn_reduJ = ctk.CTkButton(frame_2_1_2, text="Torsion Reduction", command=self.reduction_torsion, height = 20, width=ui_width/3)
+        btn_reduJ.grid(row=0, column=0, padx=0, pady=0)
+        btn_set_rz = ctk.CTkButton(frame_2_1_2, text="Set Rigidzone", command=self.set_rz, height = 20, width=ui_width/3)
+        btn_set_rz.grid(row=1, column=0, padx=0, pady=0)
+        btn_sb_nonsway = ctk.CTkButton(frame_2_1_2, text="SB,FB Nonsway", command=self.sb_nonsway, height = 20, width=ui_width/3)
+        btn_sb_nonsway.grid(row=2, column=0, padx=0, pady=0)
+
+        ##### Frame 2-2
+        frame_2_2 = ctk.CTkFrame(frame_2, width=ui_width/3)
+        frame_2_2.grid(row=0, column=1)
+
+        ##### Frame 2-3
+        frame_2_3 = ctk.CTkFrame(frame_2, width=ui_width/3)
+        frame_2_3.grid(row=0, column=2)
+
+        btn_loading = ctk.CTkButton(frame_2_3, text="Click Me to Loading", command=self.open_loading, width=ui_width/3)
         btn_loading.grid(row=3, column=0, padx=0, pady=5)
 
-        self.tb = ctk.CTkTextbox(app)
-        self.tb.grid(row=4, column=0)
+        ##### Frame 3
+        self.tb = ctk.CTkTextbox(app, width=ui_width)
+        self.tb.pack()
         sys.stdout = self # To show print messages
         sys.stderr = self # To show error messages
 
@@ -63,6 +91,8 @@ class CommonOperationUI :
         self.label_show_path = label_show_path
 
         self.Loading = None
+
+        app.mainloop()
     
     # Callback function for text
     def write(self, txt) :
@@ -82,6 +112,15 @@ class CommonOperationUI :
     
     def release_ij(self) :
         self = cmd.release_ij(self)
+
+    def reduction_torsion(self) :
+        self = cmd.reduction_torsion(self)
+
+    def set_rz(self) :
+        self = cmd.set_rz(self)
+    
+    def sb_nonsway(self) :
+        self = cmd.sb_nonsway(self)
 
     def open_loading(self) :
         self.Loading = LoadingUI(self.etabs)
